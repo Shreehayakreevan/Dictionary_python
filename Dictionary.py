c4d7ef6b8077b0c572b6baa2ccc1b04e6f1e982a
Shreehayakreevan <spideyhayak@gmail.com>
2024-05-29 20:06:48 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+#creating dictionary
+print("Indian States and its Capital")
+States_capital =  {
+    "Tamilnadu" : "Chennai",
+    "Kerala" : "Thiruvandanthapuram",
+    "Haryana" : "Punjab",
+    "West Bengal" : "Kolkata",
+    "Andra Pradesh" : "Hyderdbad"}
+print(States_capital)
